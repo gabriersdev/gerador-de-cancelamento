04eea6721b8c24256f1438a3367c55e3a24f57bf
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2023-11-02 16:40:07 -0300

    Add. códigos e scripts já desenvolvidos

---
+++ assets/js/modulos/utilitarios.js
@@ -0,0 +1,503 @@
+const isEmpty = (valor) => {
+  try{
+    if(typeof valor == 'string'){
+      return valor == undefined || valor == null || valor.trim().length <= 0;
+    }else if(Array.isArray(valor)){
+      return valor.length <= 0;
+    }else if(typeof valor == 'object'){
+      return Object.keys(valor).length <= 0;
+    }else{
+      return valor == undefined || valor == null
+    }
+  }catch(error){
+    throw new error('Ocorreu um erro ao verificar se o %s é vazio. Error: %s', typeof valor, error);
+    return true;
+  }
+}
+
+const capitalize = (valor) => {
+  return valor.charAt(0).toUpperCase() + valor.substr(1, valor.length).toLowerCase();
+}
+
+const atualizarDatas = () => {
+  const dataAtual = new Date();
+  document.querySelectorAll("[data-ano-atual]").forEach(area => {
+    area.textContent = `${dataAtual.getFullYear()}`;
+  })
+} 
+
+const controleFechamentoModal = () => {
+  const modais = document.querySelectorAll('.modal');
+  modais.forEach(modal => {
+    const btnFecha = modal.querySelector('[data-modal-fecha]');
+    btnFecha.addEventListener('click', () => {
+      $('#' + modal.id).modal('hide');
+    })
+  })
+}
+
+function sanitizarString(string){
+  if(typeof string == 'string'){
+    const substituir = [
+      {
+        original: '-',
+        subst: ''
+      },
+      {
+        original: '(',
+        subst: ''
+      },
+      {
+        original: ')',
+        subst: ''
+      },
+      {
+        original: ' ',
+        subst: ''
+      },
+    ]
+    
+    substituir.forEach(substituicao => {
+      string = string.replace(substituicao.original, substituicao.subst)
+    })
+    
+    return string.trim();
+  }else{
+    console.log('O tipo do parâmetro passado não é uma string.');
+    return null;
+  }
+}
+
+function tooltips(){
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+}
+
+function popovers(){
+  $(document).ready(function(){
+    $('[data-bs-toggle="popover"]').popover();  
+  });
+}
+
+async function SwalAlert(tipo, {icon, title, comp, confirm, html}, iconx, titlex, timer){
+  tipo = tipo.toLowerCase().trim();
+  
+  // comp: text, mensagem, timer
+  // confirm: label, showCancelButton, focusCancel
+  
+  try{
+    if(isEmpty(comp)){
+      comp = new Object();
+      comp.text = '';
+      comp.mensagem = '';
+    }
+  }catch(error){
+    throw new Error('Necessário informar o objeto para desestruturação ainda que vazio');
+  }
+  
+  try{
+    if(isEmpty(title) && !isEmpty(titlex)){
+      title = titlex;
+    }
+    
+    if(isEmpty(icon) && !isEmpty(iconx)){
+      icon = iconx;
+    }
+  }catch(error){
+    
+  }
+  
+  switch(tipo){
+    case 'confirmacao':
+    case 'confirm':
+    if(isEmpty(confirm)){
+      throw new Error('Necessário informar dados de confirmação para um alerta do tipo confirmação');
+    }else{
+      const dialog = await Swal.fire({
+        icon: icon,
+        title: title,
+        text: comp.text,
+        showCancelButton: confirm.showCancelButton,
+        confirmButtonText: confirm.label,
+        focusCancel: confirm.focusCancel,
+        timer: timer
+      })
+      
+      return new Promise((resolve, reject) => {
+        resolve({confirmed: dialog.isConfirmed});
+      });
+    }
+    break;
+    
+    case 'html':
+    if(!isEmpty(html)){
+      Swal.fire(html);
+    }else{
+      throw new Error('Objeto HTML não informado no parâmetro');
+    }
+    console.log(html)
+    break;
+    
+    case 'aviso':  
+    case 'error':
+    default:    
+    Swal.fire({
+      icon: tipo == 'error' ? 'error' : icon,
+      title: title,
+      text: comp.text,
+      footer: comp.mensagem,
+      timer: timer
+    })    
+    break;
+  }
+}
+
+function resizeTextArea(textarea){
+  // Créditos https://www.instagram.com/reel/CrdgXF3AECg/
+  const initialHeight = parseInt(getComputedStyle(textarea).getPropertyValue('height'));
+  textarea.addEventListener('input', () => {
+    textarea.style.height = `${initialHeight}px`;
+    const scrollHeight = textarea.scrollHeight;
+    const newHeight = textarea.scrollHeight - initialHeight;
+    textarea.style.height = `${newHeight < scrollHeight ? scrollHeight : newHeight}px`;
+  });
+}
+
+const copiar = async (valor) => {
+  await navigator.clipboard.writeText(valor);
+}
+
+function verificarCPF(cpf){
+  cpf = cpf.replace(/\D/g, '');
+  
+  switch (cpf){
+    case '00000000000':
+    resultado = false
+    break;
+    case '11111111111':
+    resultado = false
+    break;
+    case '22222222222':
+    resultado = false
+    break;
+    case '33333333333':
+    resultado = false
+    break;
+    case '44444444444':
+    resultado = false
+    break;
+    case '55555555555':
+    resultado = false
+    break;
+    case '66666666666':
+    resultado = false
+    break;
+    case '77777777777':
+    resultado = false
+    break;
+    case '88888888888':
+    resultado = false
+    break;
+    case '99999999999':
+    resultado = false
+    break;
+    default: 
+    if(cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+    var resultado = true;
+    [9,10].forEach(function(j){
+      var soma = 0, r;
+      cpf.split(/(?=)/).splice(0,j).forEach(function(e, i){
+        soma += parseInt(e) * ((j+2)-(i+1));
+      });
+      r = soma % 11;
+      r = (r <2)?0:11-r;
+      if(r != cpf.substring(j, j+1)) resultado = false;
+    });
+  }
+  
+  return resultado;
+}
+
+function zeroEsquerda(quantidadeZeros, valor){
+  let zeros;
+  
+  for(let i = 0; i < quantidadeZeros; i++){
+    zeros == null ? zeros = "0" : zeros = zeros + "0";
+  }
+  return (zeros + valor).slice(-quantidadeZeros);
+}
+
+function desanitizarStringURL(string){
+  if(!isEmpty(string)){
+    return decodeURIComponent(string).replaceAll('-', ' ');
+  }else{
+    return '';
+  }
+}
+
+function sanitizarStringParaURL(string){
+  if(!isEmpty(string)){
+    return encodeURIComponent(string.trim().toLowerCase().replaceAll(' ', '-'));
+  }else{
+    return '';
+  }
+}
+
+function sanitizarNumeros(string){
+  if(!isEmpty(string)){
+    return string.replace(/\D/g, '');
+  }else{
+    return '';
+  }
+}
+
+const criarEBaixarArquivo = (conteudo, nome_arquivo, ext) => {
+  try{
+    let blob = new Blob([`${JSON.parse(conteudo)}`], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, `${nome_arquivo.toUpperCase()}.${ext}`);
+  }catch(error){
+    console.warn('Framework File Saver necessário');
+    throw new Error(error);
+  }
+}
+
+const capturarDadosURL = (parametro) => {
+  try{
+    const parametros = new URLSearchParams(new URL(window.location).searchParams);
+    
+    if(isEmpty(parametro) && parametros.size > 0){
+      const saida = new Object();
+      
+      parametros.forEach((value, key) => {
+        saida[key] = value;
+      })
+      
+      return saida;
+    }else{
+      return parametros.get(parametro);
+    }
+  }catch(error){
+    throw new Error('Ocorreu um erro ao capturar os parâmetros passados na URL. Error: %s', error);
+  }
+}
+
+const format = (string, composicao) => {
+  if(string.includes('{}')){
+    if(Array.isArray(composicao)){
+      for(let i = 0; i <= (composicao.length - 1); i ++){
+        if(composicao[i] !== null && composicao[i] !== undefined){
+          string = string.replace('{}', composicao[i]);
+        }
+      }
+      return string;
+    }else if(!isNaN(composicao)){
+      return string.replace('{}', composicao);
+    }else{
+      throw new Error('Parâmetro de composição não informado');
+    }
+  }else{
+    return string;
+  }
+}
+
+const mult = (str, multiplicador) => {
+  let ret = str;
+  if(multiplicador !== undefined && typeof parseInt(multiplicador) == 'number' && !isEmpty(str)){
+    for (i = 1; i < multiplicador; i++){
+      ret += str
+    }
+  }
+  return ret
+}
+
+const tratamentoCampos = (input) => {
+  $(document).ready(function(){
+    switch(input.dataset.input){
+      case 'cpf':
+      $(input).mask('000.000.000-00', {reverse: true});
+      break;
+      
+      case 'cep':
+      $(input).mask('00000-000', {reverse: true});
+      break;
+      
+      case 'data_nascimento':
+      $(input).mask('00/00/0000');
+      break;
+      
+      case 'telefone':
+      $(input).mask('(00) 00000-0000');
+      break;
+      
+      case 'id-fid':
+      $(input).mask('000000');
+      break;
+      
+      case 'id-valor-imovel':
+      mascararValores(input);
+      break;
+      
+      case 'matricula':
+      $(input).mask('0000000');
+      break;
+      
+      default:
+      break;
+    }
+    
+    switch(input.dataset.mask){
+      case 'money':
+      mascararValores(input);
+      break;
+    }
+  });
+}
+
+function mascararValores(input){
+  // Créditos https://stackoverflow.com/questions/62894283/javascript-input-mask-currency
+  
+  if(isEmpty(input.value)){
+    input.value = 'R$ 0,00';
+  }
+  
+  input.addEventListener('input', () => {
+    const value = input.value.replace('.', '').replace(',', '').replace(/\D/g, '')
+    
+    const options = { minimumFractionDigits: 2 }
+    const result = new Intl.NumberFormat('pt-BR', options).format(parseFloat(value) / 100)
+    
+    if(isNaN(result) && result == 'NaN'){
+      input.value = 'R$ 0,00';
+    }else{
+      input.value = 'R$ ' + result;
+    }
+  })
+  input.removeAttribute('maxlength');
+}
+
+const range = ({min, max, scale}) => {
+  isEmpty(min)? min = 0 : '';
+  isEmpty(scale) ? scale = 1 : '';
+  const ret = new Array();
+  
+  if(max !== 0 && scale !== 0){
+    if(scale >= 1 && min < max){
+      for(let i = min; i < max; i += scale){
+        ret.push(i)
+      }
+    }else if(scale < 0 && min < max){
+      for(let i = min; i < max; i -= scale){
+        ret.push(i)
+      }
+    }
+    return ret;
+  }else{
+    return max;
+  }
+}
+
+const between = (value, initial, end) => {
+  return value >= initial && value <= end
+}
+
+const componenteCopiar = async () => {
+  $('[data-component-copy]').each((index, element) => {
+    const [buttonCopy, originCopy, propertyCopy] = [element.querySelector('[data-element="origin-copy"'), element.querySelector('[data-action="button-copy"]'), element.dataset.propertyCopy]
+    if(!isEmpty(originCopy) && !isEmpty(buttonCopy)){
+      $(buttonCopy).on('click', (event) => {
+        event.preventDefault();
+        return new Promise((resolve, reject) => {
+          switch(propertyCopy){
+            case 'textContent':
+            case 'innerHTML':
+            case 'outerHTML':
+            case 'value':
+              resolve(copiar(originCopy.getAttribute(propertyCopy)))
+            break;
+            default:
+              resolve(copiar(originCopy.getAttribute('value')))
+            break;
+          }
+        })
+      })
+    }else{
+      console.log('Um dos elementos necessários para o funcionamento do componente não foi definido.')
+    }
+  })
+}
+
+const setAttributes = (element, params) => {
+  if(Object.keys(params).length >= 1 && !Array.isArray(params)){
+    try{
+      let param = ''
+      for (param in params){
+        element.setAttribute(param, params[param])
+      }
+    }catch(error){
+      throw new Error('Um erro ocorreu.', error)
+    }
+  }else if(Array.isArray(params) && params.length == 2){
+    try{
+      element.setAttribute(params[0], params[1])
+    }catch(error){
+      throw new Error('Um erro ocorreu.', error)
+    }
+  }
+
+  return element;
+}
+
+const createElement = (element, params) => {
+  if(params !== null && params !== undefined){
+    element = setAttributes(element, params);
+  }
+
+  return element;
+}
+
+const criarTooltips = () => {
+  $('[data-visual-tooltip]').each((index, e) => {
+    const ele = setAttributes(e, {
+      "data-toggle": "tooltip",
+      "data-placement": "top",
+      "data-bs-custom-class": `${e.dataset.customTooltip}`
+    })
+  })
+}
+
+function primeiroNome(nome){
+  const nome_separado = nome.trim().split(' ');
+  return nome_separado[0];
+}
+
+export{
+  isEmpty,
+  capitalize,
+  atualizarDatas,
+  controleFechamentoModal,
+  sanitizarString,
+  tooltips,
+  popovers,
+  SwalAlert,
+  resizeTextArea,
+  copiar,
+  verificarCPF,
+  zeroEsquerda,
+  desanitizarStringURL,
+  sanitizarStringParaURL,
+  sanitizarNumeros,
+  criarEBaixarArquivo,
+  capturarDadosURL,
+  format,
+  mult,
+  tratamentoCampos,
+  mascararValores,
+  range,
+  between,
+  componenteCopiar,
+  createElement,
+  setAttributes,
+  criarTooltips,
+  primeiroNome
+}
---
+++ assets/js/script.js
@@ -0,0 +1,194 @@
+"use strict";
+
+import { capitalize, criarTooltips, isEmpty, popovers, primeiroNome, tooltips } from "./modulos/utilitarios.js";
+
+(() => {
+  // hljs.highlightAll();
+  
+  document.querySelectorAll('[data-recarrega-pagina]').forEach(botao => {
+    botao.addEventListener('click', () => {
+      window.location.reload();
+    })
+  })
+  
+  function atribuirLinks(){
+    const linkElementos = document.querySelectorAll('[data-link]');
+    
+    linkElementos.forEach(link => {
+      switch(link.dataset.link.toLowerCase().trim()){      
+        case 'confirmacao-cca':
+        link.href = 'https://gabrieszin.github.io/confirmacao-cca/';
+        break;
+
+        case 'password-generator':
+        link.href = 'https://gabrieszin.github.io/random-password/';
+        break;
+
+        case 'nlw-exporer':
+        link.href = 'https://github.com/gabrieszin/nlw-esports-gabrieszin';
+        break;
+
+        case 'qr-code-generator':
+        link.href = 'https://gabrieszin.github.io/qr-code-generator/';
+        break;
+        
+        case 'github-dev':
+        link.href = 'https://github.com/gabrieszin';
+        break;
+        
+        case 'github-projeto':
+        link.href = 'https://github.com/gabrieszin/gerador-de-cancelamento';
+        break;
+        
+        case 'portfolio':
+        link.href = 'https://gabrieszin.github.io/portfolio/';
+        break;  
+        
+        case 'linkedin':
+        link.href = 'https://linkedin.com/in/gabrielribeirodev/';
+        break;  
+      }
+      
+      link.setAttribute('rel', 'noopener noreferrer');
+    })
+  }
+  
+  function atribuirAcoes(){
+    const acoes = document.querySelectorAll('[data-action]');
+    
+    acoes.forEach(acao => {
+      switch(acao.dataset.action){
+        case 'acao':
+        break;
+        
+        default:
+        console.warn('A ação não foi implementada.')
+        break;
+      }
+    })
+  }
+  
+  const motivosParaSerCancelado = [
+    "Discurso de ódio",
+    "Crimes contra a Humanidade",
+    "Apropriação Cultural",
+    "Terrorismo",
+    "Bad Take",
+    "Bullying",
+    "Racismo",
+    "Machismo",
+    "Feminismo",
+    "Sexismo",
+    "Homofobia",
+    "Transfobia",
+    "Xenofobia",
+    "Intolerância religiosa",
+    "Apologia à violência",
+    "Assédio online",
+    "Cyberbullying",
+    "Compartilhamento de informações pessoais sem permissão",
+    "Assédio sexual",
+    "Uso indevido de linguagem ou memes ofensivos",
+    "Plágio",
+    "Falsas acusações",
+    "Comportamento misógino",
+    "Negligência em relação a responsabilidades éticas",
+    "Desinformação deliberada",
+    "Incitação à discriminação",
+    "Exploração de vulnerabilidades de outras pessoas",
+    "Comportamento narcisista excessivo",
+    "Cultura do linchamento online",
+    "Comportamento manipulador",
+    "Abuso de poder em posições de autoridade",
+    "Práticas antiéticas em negócios",
+    "Atos criminosos",
+    "Comentários insensíveis em momentos de tragédia",
+    "Difamação",
+    "Práticas questionáveis em marketing de mídia social",
+    "Comportamento predatório",
+    "Participação em scams ou fraudes online",
+    "Excesso de autopromoção",
+    "Falta de empatia",
+    "Ignorar ou minimizar questões importantes",
+    "Apoiar ou promover causas prejudiciais",
+    "Ataques pessoais sem motivo justificável",
+    "Abuso de poder em relações pessoais ou profissionais",
+    "Falta de transparência",
+    "Desrespeito aos direitos autorais",
+    "Compartilhamento irresponsável de informações médicas",
+    "Exploração de minorias ou grupos vulneráveis",
+    "Defesa de teorias da conspiração",
+    "Plataformas de discurso de ódio",
+    "Apoio a regimes ou grupos extremistas",
+    "Comportamento tóxico em comunidades online",
+    "Comportamento destrutivo em relacionamentos interpessoais",
+    "Abuso de animais em vídeos ou fotos",
+    "Comportamento perigoso para a saúde pública",
+    "Violação de leis de privacidade",
+    "Não se desculpar ou aprender com erros anteriores"
+  ];  
+  
+  const input = document.querySelector('.form-input-name');
+  
+  input.addEventListener('input', (evento) => {
+    evento.target.setAttribute('size', input.value.length > 0 ? input.value.length : 11)
+  })
+  
+  window.addEventListener("load", function () {
+    setTimeout(() => {
+      $('.overlay').slideUp();
+    }, 0);
+    
+    atribuirLinks();
+    atribuirAcoes();
+    criarTooltips();
+    tooltips();
+    popovers();
+    
+    let work = false;
+    
+    $('form').on('submit', (event) => {
+      event.preventDefault();
+      
+      const input = document.querySelector('.form-input-name');
+      const message = document.querySelector('#message');
+      
+      if(!isEmpty($('.form-input-name').val())){
+        // Só executará se a tarefa estiver como false
+        if(!work){
+          $(message).text('');
+          $(message).fadeIn();
+          
+          work = true; // Marcando tarefa como em execução
+          input.setAttribute('disabled', 'on');
+          
+          setTimeout(() => message.textContent = '_', 100);
+          setTimeout(() => message.innerHTML = '&nbsp;', 300);
+          setTimeout(() => message.textContent = '_', 400);
+          setTimeout(() => message.innerHTML = '&nbsp;', 500);
+          
+          setTimeout(() => {
+            // Sortear um índice do array de motivosParaSerCancelado
+            const motivoSorteado = Math.floor(Math.random() * motivosParaSerCancelado.length);
+            
+            `${capitalize(primeiroNome($(input).val()))} foi cancelado por ${motivosParaSerCancelado[motivoSorteado].toLowerCase()}.`.split('').forEach((letter, index) => {
+              setTimeout(() => {
+                index === 0 ? message.textContent = letter : message.textContent += letter;
+              }, index * 40);
+            });
+            
+            setTimeout(() => {
+              input.removeAttribute('disabled');
+              work = false;
+            }, motivosParaSerCancelado.length * 40);
+          }, 500);
+        }
+      }else{
+        $(message).fadeOut(); // Escondendo a seção de mensagem
+        $(input).val('') // Limpando o input de nome
+        input.setAttribute('size', '11'); // Resetando o input de nome para o tamanho padrão
+      }
+    })
+    
+  });
+})();
